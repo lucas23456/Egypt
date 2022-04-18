28864b4a9f7522953fbd4e4843be4642891be4b0
Glitch (leeward-nice-spectroscope) <none>
2022-04-18 14:00:21 +0000

    🐗🏟 Checkpoint ./client/scene/hall-navmesh.gltf:36613497/8100 ./client/scene/room/empty.js:36613497/43

--- client/scene/room/empty.js
+++ client/scene/room/empty.js
@@ -9,6 +9,6 @@ export function markup(
       src: 'https://cdn.glitch.global/3e6dd8ed-0bf6-4a7d-8574-aa334f54fe16/DeconstructHallGrndBlack.glb?v=1649965055990'
     }))
   scene(oom
-    .aEntity({ navMesh: true, gltfModel: '/scene/NaviMesh.glb', position: '0 0 0'})
+    .aEntity({ navMesh: true, gltfModel: '/scene/hall-navmesh.gltf', position: '0 0 0'})
     .aEntity({ gltfModel: '#HallDeconstruct', position: '0 0 0'}))
 }
