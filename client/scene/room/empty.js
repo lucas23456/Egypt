7964adb252ada1678a4ebe1e6151ebc2d3148876
Glitch (leeward-nice-spectroscope) <none>
2022-04-18 14:47:19 +0000

    🚪📊 Checkpoint ./client/scene/room/empty.js:36613497/968

--- client/scene/room/empty.js
+++ client/scene/room/empty.js
@@ -1,3 +1,28 @@
+AFRAME.registerComponent("enmap", {
+        init: function() {
+          var targetCube = new THREE.WebGLRenderTargetCube(512, 512);
+          var renderer = this.el.sceneEl.renderer;
+
+          this.el.addEventListener("model-loaded", e => {
+            let mesh = this.el.getObject3D("mesh");
+            
+            var texture = new THREE.TextureLoader().load(
+              "https://cdn.glitch.global/3e6dd8ed-0bf6-4a7d-8574-aa334f54fe16/PanoramaArt.jpg?v=1648754288986",
+              function() {
+                var cubeTex = targetCube.fromEquirectangularTexture(renderer, texture);
+                mesh.traverse(function(el) {
+                  if (el.material) {
+                    el.material.envMap = cubeTex.texture;
+                    el.material.envMap.intensity = 3;
+                    el.material.needsUpdate = true;
+                  }
+                });
+                
+              }
+            );
+          });
+        }
+      });
 export function markup(
   /** @type {import("@notml/core").oom} */ oom,
   /** @type {import("@notml/core").OOMElementProxy} */ assets,
@@ -21,6 +46,6 @@ export function markup(
     .aEntity({ navMesh: true, gltfModel: '/scene/hall-navmesh.gltf', visible: false, position: '0 0 0'})
     .aEntity({ gltfModel: '#HallDeconstruct', position: '0 0,1 0'})
   .aEntity({ gltfModel: '#Glass', position: '0 0,1 0'})
-  .aEntity({ gltfModel: '#Nft', position: '0 0,1 0', rotation: ''}))
+  .aEntity({ gltfModel: '#Nft', position: '0 0,1 0', rotation: '0 180 0'}))
   
 }
