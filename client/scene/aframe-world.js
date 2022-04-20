7f9342a98f2513a118971fa48ce8b315e98b98fa
Glitch (leeward-nice-spectroscope) <none>
2022-04-20 09:26:40 +0000

    🍇🏝 Checkpoint ./.glitch-assets:37949051/1580 ./client/scene/aframe-world.js:37949051/824

--- client/scene/aframe-world.js
+++ client/scene/aframe-world.js
@@ -8,7 +8,7 @@ export async function markup(
   assets(oom
     .img({
       id: 'grid',
-      src: 'https://cdn.glitch.global/9fc7b6f3-577d-4a75-acde-3ff7704c6420/1626899640_22-kartinkin-com-p-plitka-romb-tekstura-krasivo-23.jpg?v=1650445835764',
+      src: 'https://cdn.glitch.global/9fc7b6f3-577d-4a75-acde-3ff7704c6420/photo5372850907743632205.jpg?v=1650446509883',
       crossorigin: 'anonymous'
     })
     .img({
@@ -19,10 +19,10 @@ export async function markup(
   scene(oom
     
     .aEntity({
-      position: '0 -1.5 0',
-      geometry: 'primitive: plane; width: 10000; height: 10000;',
+      position: '0 0 0',
+      geometry: 'primitive: plane; width: 100; height: 100;',
       rotation: '-90 0 0',
-      material: 'src: #grid; repeat: 10000 10000; transparent: true; metalness:0.6; roughness: 0.4; sphericalEnvMap: #sky;'
+      material: 'src: #grid; repeat: 100 100; transparent: 1 1 1 true; metalness:0.6; roughness: 0.4; sphericalEnvMap: #sky;'
     })
     .aEntity({ light: 'color: #silver; intensity: 1; type: ambient;', visible: '' })
     /**.aEntity({ light: 'color: #ffaaff; intensity: 1.5', position: '5 5 5' })*/
