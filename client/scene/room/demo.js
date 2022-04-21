01da03281fca2359b79571b3c289deac36b773cc
Glitch (leeward-nice-spectroscope) <none>
2022-04-21 14:12:42 +0000

    💡💂 Checkpoint ./client/scene/room/demo.js:37949051/200

--- client/scene/room/demo.js
+++ client/scene/room/demo.js
@@ -6,8 +6,8 @@ export function markup(
   assets(oom
     .aAssetItem({
       id: 'art-gallery-cage',
-      src: 'https://cdn.glitch.me/9fc7b6f3-577d-4a75-acde-3ff7704c6420/CityModern.glb?v=1650532565594'
+      src: 'https://cdn.glitch.me/9fc7b6f3-577d-4a75-acde-3ff7704c6420/Multiverse - 20.04.22.glb?v=1650548839027'
     }))
   scene(oom
-    .aEntity({ gltfModel: '#art-gallery-cage', position: '0 -1.5 0', rotation: '0 180 0', scale: '0.1 0.1 0.1'}))
+    .aEntity({ gltfModel: '#art-gallery-cage', position: '0 0 0', rotation: '0 180 0', scale: '0.3 0.3 0.3'}))
 }
