e50ef17692e1b091ec33f27e583b3487aa9f4e74
Glitch (leeward-nice-spectroscope) <none>
2022-04-24 08:58:58 +0000

    🏵🐵 Checkpoint ./client/scene/room/empty.js:37949051/1

--- client/scene/room/empty.js
+++ client/scene/room/empty.js
@@ -11,7 +11,7 @@ export function markup(
 {
   assets(oom
     .aAssetItem({
-      id: 'ArtGalleryCAGE',
+      id: 'ArtGalleryCAGE1',
       src: 'https://cdn.glitch.global/9fc7b6f3-577d-4a75-acde-3ff7704c6420/shopping%20center.glb?v=1650789926055'
     })
   .aAssetItem({
