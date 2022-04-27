5628b0b7d46ebb9611f5f39a5b28da7bc359ade2
Glitch (leeward-nice-spectroscope) <none>
2022-04-27 07:13:55 +0000

    😹🐀 Checkpoint ./client/scene/room/empty.js:37949051/1835

--- client/scene/room/empty.js
+++ client/scene/room/empty.js
@@ -12,21 +12,26 @@ export function markup(
   assets(oom
     .aAssetItem({
       id: 'ArtGalleryCAGE',
-      src: 'https://cdn.glitch.global/9fc7b6f3-577d-4a75-acde-3ff7704c6420/Folds.glb?v=1650962506200'
+      src: 'https://cdn.glitch.global/9fc7b6f3-577d-4a75-acde-3ff7704c6420/ZAL.glb?v=1651041146942'
     })
   .aAssetItem({
       id: 'Glass',
       src: 'https://cdn.glitch.global/3e6dd8ed-0bf6-4a7d-8574-aa334f54fe16/GlassHall2.glb?v=1649960366473'
     })
+ .aAssetItem({
+      id: 'KAR',
+      src: 'https://cdn.glitch.me/9fc7b6f3-577d-4a75-acde-3ff7704c6420/RABOTI.glb?v=1651041419117'
+    })
   .aAssetItem({
-      id: 'Nft',
-      src: 'https://cdn.glitch.global/3e6dd8ed-0bf6-4a7d-8574-aa334f54fe16/NftPics.glb?v=1649239112623'
+      id: 'Nft1',
+      src: 'https://cdn.glitch.global/9fc7b6f3-577d-4a75-acde-3ff7704c6420/oformlenie.glb?v=1651041159752'
     }))
   
   scene(oom
-    .aEntity({ gltfModel: '#ArtGalleryCAGE', position: '-20 0 -15', scale: '1 1 1' })
-  .aEntity({ gltfModel: '#Glass', position: '0 -0.5 0', scale: '2 2 2' , rotation: '0 90 0'})
-  .aEntity({ gltfModel: '#Nft', position: '0 -1.5 0', rotation: '0 90 0', scale: '2 2 2'}))
+    .aEntity({ gltfModel: '#ArtGalleryCAGE', position: '0 0 0', scale: '1 1 1' })
+  .aEntity({ gltfModel: '#Glass', position: '0 0 0', scale: '2 2 2' , rotation: '0 90 0'})
+ .aEntity({ gltfModel: '#KAR', position: '0 0 0', scale: '1 1 1' , rotation: '0 0 0'})
+  .aEntity({ gltfModel: '#Nft', position: '0 0 0', scale: '1 1 1' , rotation: '0 0 0'}))
   
   
   
