ab96dfdaac931f20064cbd96e43b6e77efcf15be
Glitch (leeward-nice-spectroscope) <none>
2022-05-03 14:32:05 +0000

    🎩👨‍❤️‍👨 Checkpoint ./.glitch-assets:37949051/530 ./client/scene/room/demo.js:37949051/329

--- client/scene/room/demo.js
+++ client/scene/room/demo.js
@@ -6,7 +6,7 @@ export function markup(
   assets(oom
     .aAssetItem({
       id: 'Multiverse',
-      src: 'https://cdn.glitch.me/9fc7b6f3-577d-4a75-acde-3ff7704c6420/Multiverse - 20.04.22.glb?v=1650548839027'
+      src: 'https://cdn.glitch.me/9fc7b6f3-577d-4a75-acde-3ff7704c6420/Multiverse%20-%20%D0%9A%D0%B2%D0%B0%D0%BD%D1%82%D0%BE%D1%80%D0%B8%D1%83%D0%BC%2003.05.2022.glb?v=1651587631271'
     }))
   scene(oom
     .aEntity({ gltfModel: '#Multiverse', position: '0 0 0', rotation: '0 180 0', scale: '1 1 1'}))
